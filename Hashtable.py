9c04df7300bb71678471221d89152174fcda0b40
Peter Brown <phbrown@acm.org>
2025-04-22 00:45:13 -0400

    Made Hashtable behave, provided much more thorough testing.

--- Hashtable.py
+++ Hashtable.py
@@ -1,5 +1,5 @@
 import collections.abc as abc
-from typing import ClassVar
+from typing import cast, ClassVar, Iterator
 from LList import LList
 
 class Hashtable[KT, VT](abc.MutableMapping[KT, VT]):
@@ -14,9 +14,10 @@ class Hashtable[KT, VT](abc.MutableMapping[KT, VT]):
         value: int = 0
         if not isinstance(key, str):
             value = key.__hash__()
-        else: # key is a string.  Figure the hash value.
-            key = key.upper() # Ignore case
-            for c in key:
+        else: # key is a string.  Figure the hash value
+            keystr: str = cast(str, key) # type: ignore
+            keystr = keystr.upper() # Ignore case
+            for c in keystr:
                 value = value + (ord(c) - ord('A') + 1)
             value = abs(value)
         return value
@@ -24,24 +25,33 @@ class Hashtable[KT, VT](abc.MutableMapping[KT, VT]):
     def __init__(self) -> None:
         """Construct an empty hash table."""
         self._size = Hashtable.INITIAL_SIZE
-        self._table: list[LList[tuple[KT, VT]]] = [LList[tuple[KT, VT]]()] * self._size
+        self._table: list[LList[tuple[KT, VT]]] = []
+        for i in range(self._size):
+            self._table.append(LList[tuple[KT, VT]]())
+        # Post
+        assert len(self._table) == self._size
+        for i in range(self._size):
+            assert self._table[i].empty()
+            for j in range(0, i):
+                assert self._table[j] is not self._table[i]
 
     # Private helper method
-    def _resize(self) -> None:
-        """Double the size of the table.  Call this when lambda gets too high."""
+    # def _resize(self) -> None:
+    #     """Double the size of the table.  Call this when lambda gets too high."""
 
+    # Private helper method
     def _bin_idx(self, key: KT, bin_num: int) -> int:
         """Return the index of KEY in the hash chain self._table[bin_num], or -1 if
             KEY is not in that hash chain."""
         result: int = -1
-        current: LList[KT, VT] = self._table[bin_num]
+        current: LList[tuple[KT, VT]] = self._table[bin_num]
         current_idx: int = 0
         while result == -1 and not current.empty():
             if current.data()[0] == key:
                 result = current_idx
             current = current.rest()
             current_idx += 1
-        return current_idx
+        return result
 
     # QUERY METHODS
 
@@ -56,25 +66,16 @@ class Hashtable[KT, VT](abc.MutableMapping[KT, VT]):
     def __len__(self) -> int:
         """Return the number of (key, value) pairs in the hashtable."""
         return 0 # Stub! Replace with real code.
-
-    def get(self, key: KT) -> VT | None:
-        """Takes a KEY and returns the associated value, or None if KEY is not
-            in the hashtable."""
-        value: VT | None
-        try:
-            value = self[key]
-        except KeyError:
-            value = None
-        return value
     
     def __getitem__(self, key: KT) -> VT:
         """Takes a KEY and returns the associated value.  If KEY is not in the
            hashtable, raise KeyError."""
         bin_num: int = Hashtable.hashval(key) % self.tablesize()
         bin_idx: int = self._bin_idx(key, bin_num)
+        bin: LList[tuple[KT, VT]] = self._table[bin_num]
         if bin_idx == -1: # KEY is not present
             raise KeyError
-        bin: LList[KT, VT] = self._table[bin_num]
+        assert not bin.empty() and bin_idx >= 0, 'Missed KeyError: {0} {1}'.format(bin.empty(), bin_idx)
         while bin_idx > 0:
             bin = bin.rest()
             bin_idx = bin_idx - 1
@@ -82,19 +83,28 @@ class Hashtable[KT, VT](abc.MutableMapping[KT, VT]):
             assert not bin.empty() and bin_idx >= 0
         return bin.data()[1]
         
-    def __contains__(self, key: KT) -> bool:
-        """Returns True if KEY is in the hashtable, or False if not."""
-        contained: bool = False
-        try:
-            self[key]
-            contained = True
-        except KeyError:
-            pass
-        return contained
+    # This is a mixin from Mapping
+    # def __contains__(self, keyobj: object) -> bool:
+    #     """Returns True if KEY is in the hashtable, or False if not."""
+    #     contained: bool = False
+    #     key: KT = cast(KT, keyobj)
+    #     try:
+    #         self[key]
+    #         contained = True
+    #     except KeyError:
+    #         pass
+    #     return contained
     
-    def __iter__(self) -> None:
+    def __iter__(self) -> Iterator[KT]:
         """Returns an iterator over the keys in the hashtable."""
-        raise NotImplementedError
+        for bin in self._table:
+            if bin.empty():
+                continue
+            else:
+                current = bin
+                while not current.empty():
+                    yield current.data()[0]
+                    current = current.rest()
 
     # MUTATOR METHODS
 
--- TestHashtable.py
+++ TestHashtable.py
@@ -9,6 +9,17 @@ class TestHashtable(unittest.TestCase):
     def setUp(self) -> None:
         self._empty = Hashtable[str, str]()
 
+        self._1item = Hashtable[str, str]()
+        self._1item['Zelle'] = 'John'
+
+        self._6items = Hashtable[str, str]()
+        self._6items['Barrera'] = 'Joseph'
+        self._6items['Brown'] = 'Peter'
+        self._6items['Mangum'] = 'Amanda'
+        self._6items['Nasiri'] = 'Moloud'
+        self._6items['Sorrells'] = 'Jessica'
+        self._6items['Zelle'] = 'John'
+
     def testHashA(self) -> None:
         self.assertEqual(Hashtable.hashval('a'), 1)
 
@@ -21,17 +32,108 @@ class TestHashtable(unittest.TestCase):
     def testHash40(self) -> None:
         self.assertEqual(Hashtable.hashval('40'), 28)
 
+    def testHashBarrera(self) -> None:
+        self.assertEqual(Hashtable.hashval('Barrera'), 63)
+
+    def testHashBrown(self) -> None:
+        self.assertEqual(Hashtable.hashval('Brown'), 72)
+
+    def testHashMangum(self) -> None:
+        self.assertEqual(Hashtable.hashval('Mangum'), 69)
+
+    def testHashNasiri(self) -> None:
+        self.assertEqual(Hashtable.hashval('Nasiri'), 70)
+
+    def testHashSorrells(self) -> None:
+        self.assertEqual(Hashtable.hashval('Sorrells'), 118)
+
     def testTableSizeInit(self) -> None:
         self.assertEqual(self._empty.tablesize(), Hashtable.INITIAL_SIZE)
+        self.assertEqual(self._1item.tablesize(), Hashtable.INITIAL_SIZE)
+        self.assertEqual(self._6items.tablesize(), Hashtable.INITIAL_SIZE)
+
+    def testGetitemZelleEmpty(self) -> None:
+        with self.assertRaises(KeyError):
+            self._empty['Zelle']
 
     def testGetZelleEmpty(self) -> None:
         self.assertEqual(self._empty.get('Zelle'), None)
 
     def testGetZellePresent(self) -> None:
-        self._empty.put('Zelle', 'John')
-        self.assertEqual(self._empty.get('Zelle'), 'John')
+        self.assertEqual(self._1item.get('Zelle'), 'John')
 
+    def testGetZellePresentBrackets(self) -> None:
+        self.assertEqual(self._1item['Zelle'], 'John')
 
+    def testGet5Items(self) -> None:
+        keys: list[str] = ['Barrera', 'Brown', 'Mangum', 'Nasiri', 'Sorrells', 'Zelle']
+        values: list[str] = ['Joseph', 'Peter', 'Amanda', 'Moloud', 'Jessica', 'John']
+        for i in range(len(keys)):
+            with self.subTest(key=keys[i]):
+                self.assertEqual(self._6items[keys[i]], values[i])
+        with self.assertRaises(KeyError):
+            self._6items['Hopkins']
+
+    def testReplacement(self) -> None:
+        self.assertEqual(self._6items['Brown'], 'Peter')
+        self._6items['Brown'] = 'Laura'
+        self.assertEqual(self._6items['Brown'], 'Laura')
+
+    def testChaining(self) -> None:
+        self.assertEqual(Hashtable.hashval('Barrera') % self._6items.tablesize(), 3)
+        self.assertEqual(Hashtable.hashval('Brown') % self._6items.tablesize(), 0)
+        self.assertEqual(Hashtable.hashval('Mangum') % self._6items.tablesize(), 1)
+        self.assertEqual(Hashtable.hashval('Nasiri') % self._6items.tablesize(), 2)
+        self.assertEqual(Hashtable.hashval('Sorrells') % self._6items.tablesize(), 2)
+        self.assertEqual(Hashtable.hashval('Zelle') % self._6items.tablesize(), 0)
+
+    def testBinIdx(self) -> None:
+        self.assertEqual(self._6items._bin_idx('Barrera', 0), -1)
+        self.assertEqual(self._6items._bin_idx('Barrera', 1), -1)
+        self.assertEqual(self._6items._bin_idx('Barrera', 2), -1)
+        self.assertEqual(self._6items._bin_idx('Barrera', 3), 0)
+
+        self.assertEqual(self._6items._bin_idx('Brown', 0), 1)  # Same chain as Zelle
+        self.assertEqual(self._6items._bin_idx('Brown', 1), -1)
+        self.assertEqual(self._6items._bin_idx('Brown', 2), -1)
+        self.assertEqual(self._6items._bin_idx('Brown', 3), -1)
+
+        self.assertEqual(self._6items._bin_idx('Mangum', 0), -1)
+        self.assertEqual(self._6items._bin_idx('Mangum', 1), 0)
+        self.assertEqual(self._6items._bin_idx('Mangum', 2), -1)
+        self.assertEqual(self._6items._bin_idx('Mangum', 3), -1)
+
+        self.assertEqual(self._6items._bin_idx('Nasiri', 0), -1)
+        self.assertEqual(self._6items._bin_idx('Nasiri', 1), -1)
+        self.assertEqual(self._6items._bin_idx('Nasiri', 2), 1) # Same chain as Sorrells
+        self.assertEqual(self._6items._bin_idx('Nasiri', 3), -1)
+
+        self.assertEqual(self._6items._bin_idx('Sorrells', 0), -1)
+        self.assertEqual(self._6items._bin_idx('Sorrells', 1), -1)
+        self.assertEqual(self._6items._bin_idx('Sorrells', 2), 0)
+        self.assertEqual(self._6items._bin_idx('Sorrells', 3), -1)
+
+        self.assertEqual(self._6items._bin_idx('Zelle', 0), 0)
+        self.assertEqual(self._6items._bin_idx('Zelle', 1), -1)
+        self.assertEqual(self._6items._bin_idx('Zelle', 2), -1)
+        self.assertEqual(self._6items._bin_idx('Zelle', 3), -1)
+
+    # Test mixins
+    def testKeys(self) -> None:
+        self.assertEqual(list(self._empty.keys()), [])
+        self.assertEqual(list(self._1item.keys()), ['Zelle'])
+        self.assertEqual(list(self._6items.keys()), ['Zelle', 'Brown', 'Mangum', 'Sorrells', 'Nasiri', 'Barrera'])
+
+    def testContains(self) -> None:
+        keys = ['Zelle', 'Brown', 'Mangum', 'Sorrells', 'Nasiri', 'Barrera']
+        for key in keys:
+            with self.subTest(key=key):
+                self.assertFalse(key in self._empty)
+                self.assertTrue(key in self._6items)
+        self.assertTrue('Zelle' in self._1item)
+        for key in keys[1:]:
+            with self.subTest(key=key):
+                self.assertFalse(key in self._1item)
 
 if __name__ == '__main__':
     unittest.main()
